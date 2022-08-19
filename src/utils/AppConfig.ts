189ffe15c0b6fcb8f871b509bf4598d4066192fb
shin <kashiashin@gmail.com>
2022-08-19 14:51:02 +0900

    ga2

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'two gather',
+  site_name: 'Two Gatther',
+  title: 'Two Gather',
   canonical: 'https://twogather.queueandmusic.com',
   description: '음악을 즐기는 1만가지 방법',
   locale: 'ko',
